c0737ef8f90758d0f66ad3af436b9f7fa456079f
Dilan Utria <uc.dylan@gmail.com>
2025-03-01 12:34:16 -0500

    Función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // Array para almacenar los nombres de los amigos
 let amigos = [];
+
+function agregarAmigo() {
+    // Obtener el valor del campo de texto
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    // Validar que el campo no esté vacío
+    if (!nombre) {
+        alert('Por favor, ingresa un nombre');
+        return;
+    }
+
+    //Agregar el nombre al array y actualizar la lista
+    amigos.push(nombre);
+    actualizarLista();
+
+    // Limpiar el campo de texto
+    input.value = '';
+
+}
